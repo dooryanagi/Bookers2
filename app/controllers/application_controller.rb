258e10aaa03169c8632d1287f97bec4203253f30
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-17 07:46:55 +0000

    [Finish]ユーザー認証機能の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  # 【?】ifの：の意味
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # privateとは異なり他のコントローラからも参照できる
+  protected
+  
+  # メソッドとして定義することでbefore_actionを用いれるようになる
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.permitメソッド、サインイン時にkeyのデータ操作を許可している
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
 end
